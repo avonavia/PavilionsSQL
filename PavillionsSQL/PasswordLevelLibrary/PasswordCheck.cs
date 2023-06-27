462c03f09602e5b2268b95f66a73dc2317669b83
avonavia <121552637+avonavia@users.noreply.github.com>
2023-06-27 12:16:05 +0300

    Add files via upload

---
+++ PavillionsSQL/PasswordLevelLibrary/PasswordCheck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordLevelLibrary
+{
+    public class PasswordCheck
+    {
+        public int CheckPassword(string password)
+        {
+            bool hasNumbers = false;
+            bool hasSymbols = false;
+
+            string numbers = "1234567890";
+            string symbols = "@/?.,!&";
+
+            int level = 0;
+
+            if (password.Length >= 8)
+            {
+                level++;
+            }
+            if (password.Any(Char.IsUpper) && password.Any(Char.IsLower))
+            {
+                level++;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (password.Contains(numbers[i]))
+                {
+                    hasNumbers = true;
+                }
+            }
+            if (hasNumbers)
+            {
+                level++;
+            }
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                if (password.Contains(symbols[i]))
+                {
+                    hasSymbols = true;
+                }
+            }
+            if (hasSymbols)
+            {
+                level++;
+            }
+            return level;
+        }
+    }
+}
